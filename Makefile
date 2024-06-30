40c8b17e55b47c7433f330c5b3f0b077cc391ed9
JFKongphop <kongphopleo@gmail.com>
2024-06-30 23:14:05 +0700

    for example

---
+++ Makefile
@@ -0,0 +1,17 @@
+1:
+	node dist/01-viem.js
+
+2:
+	node dist/02-viem.js
+
+3:
+	node dist/03-viem.js
+
+4:
+	node dist/04-viem.js
+
+5:
+	node dist/05-viem.js
+
+6:
+	node dist/06-viem.js
--- src/01-viem.ts
+++ src/01-viem.ts
@@ -1,1 +1,22 @@
-console.log("hello world");
+import dotenv from 'dotenv';
+import { Address, createPublicClient, http, formatEther } from 'viem'
+import { holesky } from 'viem/chains'
+import { privateKeyToAccount } from 'viem/accounts'
+
+ 
+dotenv.config();
+
+const publicClient = createPublicClient({ 
+  chain: holesky,
+  transport: http()
+})
+
+const account = privateKeyToAccount(process.env.PRIVATE_KEY as Address)
+
+
+publicClient.
+  getBalance({
+    address: account.address,
+}).then((data)  => console.log(formatEther(data)))
+
+
